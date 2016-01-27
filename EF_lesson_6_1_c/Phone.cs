b48e9b0188d3baf4b8b386d0ff1b148a0e7d84f5
Alex <alexis1784@gmail.com>
2016-01-27 20:26:46 +0500

    working versio

---
+++ EF_lesson_6_1_c/Phone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_lesson_6_1_c
+{
+    public class Phone
+    {
+        public int Ident { get; set; }
+        public string Name { get; set; }
+        public int Discount { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ EF_lesson_6_1_c/PhoneContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_lesson_6_1_c
+{
+    
+    class FluentContext : DbContext
+    {
+        public FluentContext()
+            : base("EF_lesson_6_1_c")
+        { }
+
+        public DbSet<Phone> Phones { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Phone>().ToTable("Mobiles");
+            modelBuilder.Entity<Phone>().HasKey(p => p.Ident);
+            modelBuilder.Entity<Phone>().Ignore(p => p.Discount);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
